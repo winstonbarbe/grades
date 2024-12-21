96f8c23f364b270f60189093538144d36d06056c
winstonbarbe <winston.barbe@gmail.com>
2024-12-21 17:01:44 -0500

    adds solves for all tests

--- grades.rb
+++ grades.rb
@@ -1,11 +1,23 @@
 def final_grade(array)
   # This method currently has no code.
   # Your job is to write the code to get the method to work!
-  scores = array
-  scores_sum = scores.sum
-  scores_avg = scores_sum / 4
-  if scores_avg >=90
+  if array.empty?
+    return "I"
+  else
+    scores = array
+    scores_sum = scores.sum
+    scores_avg = (scores_sum / scores.length).round
+  end
+  if scores_avg >= 90
     "A"
+  elsif scores_avg >= 80
+    "B"
+  elsif scores_avg >= 70
+    "C"
+  elsif scores_avg >= 60
+    "D"
+  else
+    "F"
   end
 
 end
--- test_grades.rb
+++ test_grades.rb
@@ -23,56 +23,56 @@ else
 end
 
 # # Test 3
-# puts
-# puts "TEST #3: Typical C grade"
-# if final_grade([70.0, 90.0, 72.0, 77.0]) == "C"
-# 	puts "TEST #3 passed"
-# else
-# 	puts "TEST #3 failed"
-# end
+puts
+puts "TEST #3: Typical C grade"
+if final_grade([70.0, 90.0, 72.0, 77.0]) == "C"
+	puts "TEST #3 passed"
+else
+	puts "TEST #3 failed"
+end
 
-# # Test 3
-# puts
-# puts "TEST #3: Typical C grade"
-# if final_grade([70.0, 90.0, 72.0, 77.0]) == "C"
-# 	puts "TEST #3 passed"
-# else
-# 	puts "TEST #3 failed"
-# end
+# Test 3
+puts
+puts "TEST #3: Typical C grade"
+if final_grade([70.0, 90.0, 72.0, 77.0]) == "C"
+	puts "TEST #3 passed"
+else
+	puts "TEST #3 failed"
+end
 
-# # Test 4
-# puts
-# puts "TEST #4: Typical D grade"
-# if final_grade([60.0, 90.0, 62.0, 64.0]) == "D"
-# 	puts "TEST #4 passed"
-# else
-# 	puts "TEST #4 failed"
-# end
+# Test 4
+puts
+puts "TEST #4: Typical D grade"
+if final_grade([60.0, 90.0, 62.0, 64.0]) == "D"
+	puts "TEST #4 passed"
+else
+	puts "TEST #4 failed"
+end
 
-# # Test 5
-# puts
-# puts "TEST #5: Typical F grade"
-# if final_grade([55.0, 43.0, 88.0, 20.0, 60.0]) == "F"
-# 	puts "TEST #5 passed"
-# else
-# 	puts "TEST #5 failed"
-# end
+# Test 5
+puts
+puts "TEST #5: Typical F grade"
+if final_grade([55.0, 43.0, 88.0, 20.0, 60.0]) == "F"
+	puts "TEST #5 passed"
+else
+	puts "TEST #5 failed"
+end
 
-# # Test 6
-# puts
-# puts "TEST #6: Student took no tests"
-# if final_grade([]) == "I"
-# 	puts "TEST #6 passed"
-# else
-# 	puts "TEST #6 failed"
-# end
+# Test 6
+puts
+puts "TEST #6: Student took no tests"
+if final_grade([]) == "I"
+	puts "TEST #6 passed"
+else
+	puts "TEST #6 failed"
+end
 
-# # Test 7
-# puts
-# puts "TEST #7: Ensure that rounding is taking place"
-# if final_grade([88.0, 90.0, 85.0, 95.0]) == "A"
-# 	puts "TEST #7 passed"
-# else
-# 	puts "TEST #7 failed"
-# 	puts "(NOTE: The average is 89.5, which should round up to 90.)"
-# end
+# Test 7
+puts
+puts "TEST #7: Ensure that rounding is taking place"
+if final_grade([88.0, 90.0, 85.0, 95.0]) == "A"
+	puts "TEST #7 passed"
+else
+	puts "TEST #7 failed"
+	puts "(NOTE: The average is 89.5, which should round up to 90.)"
+end
